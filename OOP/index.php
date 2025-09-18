fc82d52977162021cdfbee240f225b08f50f9f32
fauzirahmat89 <fauzirahmat89@gmail.com>
2025-09-18 20:00:25 +0700

    Tugas oop

--- OOP/index.php
+++ OOP/index.php
@@ -1,58 +1,27 @@
 <?php
-    // contoh class
-    class Mobil {
-        public $roda = 4; // property
-        protected $rodaProtected = 8;
-        private $rodaPrivate = 6;
-        public $merek = "";
+    require_once('./animal.php');
+    require_once("./Frog.php");
+    require_once('./Ape.php');
 
-        public function __construct($merek){
-            $this->merek= $merek;
-        }
-        
-        public function jalan(){ // method
-            echo "Mobil berjalan <br>";
-        }
-        private function jalanPrivate(){ // method
-            echo "Mobil berjalan <br>";
-        }
-        public function jumlah_roda(){
-            echo "jumlah roda : " . $this->roda . "<br>"; // gunakan $this jika ingin memanggil property didalam class atau method
-        }
+    $sheep = new Animal("shaun");
+    echo "Name : " . $sheep->name . "<br>";
+    echo "legs : " . $sheep->legs . "<br>";
+    echo "cold blooded: " . $sheep->cold_blooded . "<br><br>";
 
-    }
+    // release 1
+    $kodok = new Frog("buduk");
+    echo "Name : " . $kodok->name . "<br>";
+    echo "legs : " . $kodok->legs . "<br>";
+    echo "cold blooded: " . $kodok->cold_blooded . "<br>";
+    echo "Jump : "; $kodok->jump(); echo "<br><br>";
 
-    $avanza = new Mobil("avanza"); // membuat objek (instansiasi)
+    $sungokong = new Ape("kera sakti");
+    echo "Name : " . $sungokong->name . "<br>";
+    echo "legs : " . $sungokong->legs . "<br>";
+    echo "cold blooded: " . $sungokong->cold_blooded . "<br>";
+    echo "Yell : "; $sungokong->yell(); echo "<br><br>";
 
-    $avanza->jalan(); // memanggil method
-    echo "rodanya : " . $avanza->roda . "<br>"; // memanggil property
-    echo $avanza -> jumlah_roda(); // memanggil method jumlah_roda
 
-    // visibilitas ada private protected public dan default
-    $avanzaPrivate = new Mobil("avanzaPrivate");
-    // echo $avanzaPrivate->jalanPrivate(); // akan error karna private methodnya
-    // echo $avanzaPrivate->rodaPrivate; // property atau method private hannya dapat diakses didalam classnya tidak bisa diluar class
 
-    // Protected
-    // property atau method portected hannya dapat diakses
-    // jika class tersebut turunannya tetapi akses diluar
-    // classnya tidak bisa
-    class MobilSport extends Mobil {
-        protected $maxSpeed;
-        public function tampil(){
-            echo $this->rodaProtected;
-        }
-        // public function tampil(){
-        //     echo $this->rodaPrivate; // error tidak bisa karena private
-        // }
-    }
-
-    $ferrari = new MobilSport("ferari");
-    echo $ferrari->tampil();
-
-    // CONTRUKTOR
-    // contruktor adalah method atau function yang akan dijalankan
-    // pertama kali
-    $xenia = new Mobil("Xenia");
-    echo "<br>" . $xenia->merek;
+    
 ?>
